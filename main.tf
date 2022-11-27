e7f477521606da9c790bd6acaa8a0a0d9091e707
Olalekan Fadele <os.fadele@gmail.com>
2022-11-28 00:49:44 +0100

    Added setup files

---
+++ main.tf
@@ -0,0 +1,42 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+terraform {
+  cloud {
+    organization = "Thrinkshive"
+
+    workspaces {
+      name = "CRC_FRONTEND"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "keenson-bk"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
