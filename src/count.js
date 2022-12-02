5cddeede5e23902a72ae9127e675067445341926
Olalekan Fadele <keensono@gmail.com>
2022-12-02 14:56:35 +0100

    Static files (#7)
    * Added static&terraform files
    
    * Updated api link
    
    * Updated tests workflow
    
    * Updated tests workflow
    
    Co-authored-by: Olalekan Fadele <os.fadele@gmail.com>

---
+++ src/count.js
@@ -0,0 +1,24 @@
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+    });
+    
+    
+    const apiGateway = 'https://0tccbk0gn8.execute-api.us-east-1.amazonaws.com/';
+    
+    const getVisitCount = () => {
+    let count = 1;
+    //   fetch(apiGateway)
+    //     .then(response => {
+    //       return response.json()
+    //     })
+    //     .then(response => {
+    //       count = response;
+        console.log("You are visitor number - " + count);
+    //       document.getElementById('visitorCount').innerText = count;
+    //     }).catch(function (error) {
+    //       console.log(error);
+    //     });
+    //   return count;
+    } 
